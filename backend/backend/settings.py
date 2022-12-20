4f3c11e75055ea6bdf64ec01e63304a287b1ad51
HungHungGNX <79443809+HungHungGNX@users.noreply.github.com>
2022-12-20 11:13:23 +0700

    Fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,6 +123,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE':'django.db.backends.postgresql',
+        # 'NAME':'hmvnnziw',
+        # 'USER':'hmvnnziw',
+        # 'PASSWORD':'kTNpSfP_hftClV6p-ORrQodtaek4TIkW',
+        # 'HOST':'john.db.elephantsql.com',
+        # 'PORT':'5432'
     }
 }
 
